49783d0cd7a490f4fbc19fe538e8885bdd0ff28c
Atlasco <106603316+RehamATE55@users.noreply.github.com>
2022-11-07 14:53:36 +0200

    Update models.py

--- stock_tree_serial_number/models/models.py
+++ stock_tree_serial_number/models/models.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class serialnumstock(models.Model):
     _inherit = 'stock.move.line'
 
-    serial_numb = fields.Char(string="Serial Num", required=False, )
+    serial_numb = fields.Char(string="Vendor Serial Num", required=False, )
